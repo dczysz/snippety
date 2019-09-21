79a8cae6fc206c1a0c88b3fc4f479b0f1962b2c5
D. Czysz <=>
2019-09-20 19:42:53 -0600

    Somewhat implemented tabs in editor

--- src/components/CodeInput.js
+++ src/components/CodeInput.js
@@ -8,14 +8,35 @@ import StyledCodeInput from './styles/CodeInput';
 const CodeInput = () => {
   const [state, dispatch] = useContext(AppContext);
 
+  const filterInput = e => {
+    // Handle tabs
+    if (e.keyCode === 9) {
+      e.preventDefault();
+
+      const currentInput = e.target.value;
+      const cursorStart = e.target.selectionStart;
+      const cursorEnd = e.target.selectionEnd;
+      const newOutput =
+        currentInput.substring(0, cursorStart) +
+        '  ' +
+        currentInput.substring(cursorEnd);
+
+      dispatch({ type: actionTypes.INPUT, payload: newOutput });
+
+      //TODO: Fix tab moving cursor to end of textarea
+      // e.target.cursorStart = cursorStart; //!Doesnt work
+    }
+  };
+
   return (
     <StyledCodeInput showHint={state.input === ''}>
       <TitleBar title="Input" />
       <textarea
         value={state.input}
-        onChange={e =>
-          dispatch({ type: actionTypes.INPUT, payload: e.target.value })
-        }
+        onChange={e => {
+          dispatch({ type: actionTypes.INPUT, payload: e.target.value });
+        }}
+        onKeyDown={filterInput}
         spellCheck={false}
         aria-label="Code Input"
       />
